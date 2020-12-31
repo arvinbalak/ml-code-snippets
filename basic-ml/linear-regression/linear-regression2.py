c548f536e89165e25e3d02edb2eec7e081d85685
Aravind Bk <aravindbk92@gmail.com>
2020-12-31 17:12:16 -0500

    lin regression2

--- basic-ml/linear-regression/linear-regression2.py
+++ basic-ml/linear-regression/linear-regression2.py
@@ -25,16 +25,18 @@ def gradient_descent(X, y, theta, alpha=0.01, iterations=1000):
 
 # ---Get data---
 data_file = pathlib.Path(__file__).resolve().parent.parent / \
-    'data/andrewng/ex1data1.txt'
-data = pd.read_csv(data_file, header=None, names=['Population', 'Profit'])
+    'data/andrewng/ex1data2.txt'
+data = pd.read_csv(data_file, header=None, names=['Size', 'Bedroom', 'Price'])
+# print(data.head())
 # print(data.describe())
-# data.plot(kind='scatter', x='Population', y='Profit', figsize=(12, 8))
 
 # ---Prepare data---
+data = (data - data.mean()) / data.std()
 data.insert(0, 'Ones', 1)
 X = data.iloc[:, 0:data.shape[1]-1]
 y = data.iloc[:, data.shape[1]-1]
 # print(X.head())
+# print(y.head())
 
 X = X.to_numpy()
 y = y.to_numpy().reshape(-1, 1)
@@ -49,20 +51,10 @@ print("Initial cost: {}".format(initial_cost))
 print("Final cost: {}".format(final_cost))
 print("Final theta: \n{}".format(final_theta))
 
-# ---Plot---
-x = np.linspace(data.Population.min(), data.Population.max(), 100)
-prediction = final_theta[0] + (final_theta[1] * x)
-
-fig, ax = plt.subplots(1, 2, figsize=(12, 8))
-ax[0].plot(x, prediction, 'r', label='Prediction')
-ax[0].scatter(data.Population, data.Profit, label='Training Data')
-ax[0].legend(loc=2)
-ax[0].set_xlabel('Population')
-ax[0].set_ylabel('Profit')
-ax[0].set_title('Predicted Profit vs. Population Size')
-
-ax[1].plot(cost_list, 'r')
-ax[1].set_xlabel('Iteration')
-ax[1].set_ylabel('Cost')
-ax[1].set_title('Error vs Training Epoch')
+# # ---Plot---
+fig, ax = plt.subplots(figsize=(12, 8))
+ax.plot(cost_list, 'r')
+ax.set_xlabel('Iteration')
+ax.set_ylabel('Cost')
+ax.set_title('Error vs Training Epoch')
 plt.show()
